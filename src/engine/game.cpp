eb804e6afb70e1c476847e9ceaec06683f57f5bf
Diego Seabra <dss-diego@hotmail.com>
2021-10-06 08:39:24 -0300

    game scene

--- src/engine/game.cpp
+++ src/engine/game.cpp
@@ -5,17 +5,30 @@
 #include "game.h"
 
 void Game::init() {
-
+    // TODO: Initialize scene vector
 }
 
 void Game::draw() {
-
+    // REVIEW: Draw current scene (?)
 }
 
 void Game::update() {
-
+    // REVIEW: Update current scene (?)
 }
 
 void Game::cleanUp() {
+    // Remove all scenes before finishing the game
+    mScenes.clear();
+}
+
+void Game::addScene(Scene scene) {
+
+}
+
+void Game::removeScene(Scene scene) {
+
+}
+
+void Game::goToScene(Scene scene) {
 
 }
--- src/engine/game.h
+++ src/engine/game.h
@@ -4,10 +4,22 @@
 
 #pragma once
 
+#include <vector>
+#include "scene.h"
+
 class Game {
 public:
     virtual void init();
     virtual void draw();
     virtual void update();
     virtual void cleanUp();
+private:
+    std::vector<Scene> mScenes;
+    Scene mCurrentScene;
+
+    void addScene(Scene scene);
+    void removeScene(Scene scene);
+//    void removeScene(int sceneIdx);
+    void goToScene(Scene scene);
+//    void goToScene(int sceneIndex);
 };
