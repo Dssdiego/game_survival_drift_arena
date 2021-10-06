2fb8c32910856f1dbc4f5430c4634a820d1e2b28
Diego Seabra <dss-diego@hotmail.com>
2021-10-06 08:39:34 -0300

    play scene testing

---
+++ src/games/pong/code/scenes/playscene.cpp
@@ -0,0 +1,15 @@
+//
+// Created by dss-d on 06/10/2021.
+//
+
+#include "playscene.h"
+
+PlayScene::PlayScene() {
+    mPaddleLeft = Entity();
+    mPaddleRight = Entity();
+    mBall = Entity();
+
+    addEntity(mPaddleLeft);
+    addEntity(mPaddleRight);
+    addEntity(mBall);
+}
---
+++ src/games/pong/code/scenes/playscene.h
@@ -0,0 +1,14 @@
+//
+// Created by dss-d on 06/10/2021.
+//
+
+#pragma once
+
+#include "../../../../engine/scene.h"
+
+class PlayScene : public Scene {
+    PlayScene();
+
+private:
+    Entity mPaddleLeft, mPaddleRight, mBall;
+};
