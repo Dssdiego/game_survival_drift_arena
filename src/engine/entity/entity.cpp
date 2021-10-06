8df1be42125fb0ac7e52c9b3ce11ae451ebb2a8b
Diego Seabra <dss-diego@hotmail.com>
2021-10-05 22:16:34 -0300

    entity with position and texture

---
+++ src/engine/entity/entity.cpp
@@ -0,0 +1,8 @@
+//
+// Created by dss-d on 05/10/2021.
+//
+
+#include "entity.h"
+
+Entity::Entity(Vec2 position, Texture texture) : position({0.0f,0.0f}) {
+}
---
+++ src/engine/entity/entity.h
@@ -0,0 +1,25 @@
+//
+// Created by dss-d on 05/10/2021.
+//
+
+#pragma once
+
+#include "../maths/vector2.h"
+#include "../graphics/texture.h"
+
+// REVIEW: Make something like a transform for future uses?
+//struct Transform {
+//    Vec2 position;
+//};
+
+class Entity {
+public:
+    Entity(Vec2 position, Texture texture);
+
+private:
+    Vec2 position;
+    Texture texture;
+
+//    void update();
+//    void draw();
+};
